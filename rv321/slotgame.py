ef9fbd5ef1face66925375f7c22721ab25a454d0
talisman <s.k.recode226@gmail.com>
2020-07-30 16:45:12 +0900

    a

--- rv321/slotgame.py
+++ rv321/slotgame.py
@@ -48,16 +48,16 @@ tmp_memory_addr = 17
 program_main = [
     # 初期化
     Inst.LUI(seg_led, 0x04000000),
-    Inst.LW(push_switch,seg_led,0x48),
     Inst.ADDI(slot1_counter, 0, slot1_init),
     Inst.ADDI(slot2_counter, 0, slot2_init),
     Inst.ADDI(slot3_counter, 0, slot3_init),
+    Inst.LW(push_switch,seg_led,0x48),
     Inst.ADDI(step,0,0),
     Inst.ADDI(counter_max, 0, 9),
     # ループ部分
     "input",
     Inst.ANDI(push_result,push_switch,0x01),
-    Inst.LBNE(push_result,1,"slot1"),
+    Inst.LBEQ(push_result,0,"slot1"),
     Inst.ADDI(step,step,1),
     "slot1",
     Inst.LBGE(step,1,"slot2"),
