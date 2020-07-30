1580e4cad3753cc2491747d3ffa19fd9c19e78ee
talisman <s.k.recode226@gmail.com>
2020-07-30 15:57:39 +0900

    hex書き出し

--- rv321/slotgame.py
+++ rv321/slotgame.py
@@ -5,7 +5,7 @@
 
 from inst import Inst, asm, print_asm, print_ihex
 
-#seg led pattern
+# seg led pattern
 
 list_seg_patterns = [0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0, 0xFE, 0xF6]
 seg_patterns_mem = 5
@@ -18,11 +18,11 @@ program = [
 
 
 for i in range(10):
-    program.add(Inst.ADDI(seg_pattern, 0, list_seg_patterns[i]))
-    program.add(Inst.SB(seg_patterns_mem, seg_pattern, i))
-    program.add(Inst.SB(seg_patterns_mem, seg_pattern, i + 1))  # dummySB
+    program.append(Inst.ADDI(seg_pattern, 0, list_seg_patterns[i]))
+    program.append(Inst.SB(seg_patterns_mem, seg_pattern, i))
+    program.append(Inst.SB(seg_patterns_mem, seg_pattern, i + 1))  # dummySB
 
-#slot main
+# slot main
 
 seg_led = 6
 counter_max = 7
@@ -40,7 +40,7 @@ slot2_memory = 23
 slot3_init = 0x5
 slot3_counter = 31
 slot3_memory_addr = 32
-slot3_memory_addr = 33
+slot3_memory = 33
 
 program_main = [
     # Main
@@ -48,48 +48,41 @@ program_main = [
     Inst.ADDI(slot1_counter, 0, slot1_init),  # x7 = 0 (counter)
     Inst.ADDI(slot2_counter, 0, slot2_init),  # x7 = 0 (counter)
     Inst.ADDI(slot3_counter, 0, slot3_init),  # x7 = 0 (counter)
-    Inst.ADDI(counter_max,0,9),
-
+    Inst.ADDI(counter_max, 0, 9),
     "loop",
-    Inst.ADD(slot1_memory_addr, slot1_counter, seg_patterns_mem),  
-    Inst.LBU(slot1_memory, slot1_memory_addr, 0),  
+    Inst.ADD(slot1_memory_addr, slot1_counter, seg_patterns_mem),
+    Inst.LBU(slot1_memory, slot1_memory_addr, 0),
     Inst.SB(seg_led, slot1_memory, 0),  # dummySB
     Inst.SB(seg_led, slot1_memory, 0),
-
-    Inst.ADD(slot2_memory_addr, slot2_counter, seg_patterns_mem),  
-    Inst.LBU(slot2_memory, slot2_memory_addr, 0),  
+    Inst.ADD(slot2_memory_addr, slot2_counter, seg_patterns_mem),
+    Inst.LBU(slot2_memory, slot2_memory_addr, 0),
     Inst.SB(seg_led, slot2_memory, 1),  # dummySB
     Inst.SB(seg_led, slot2_memory, 1),
-
-    Inst.ADD(slot3_memory_addr, slot3_counter, seg_patterns_mem),  
-    Inst.LBU(slot3_memory, slot3_memory_addr, 0),  
+    Inst.ADD(slot3_memory_addr, slot3_counter, seg_patterns_mem),
+    Inst.LBU(slot3_memory, slot3_memory_addr, 0),
     Inst.SB(seg_led, slot3_memory, 2),  # dummySB
     Inst.SB(seg_led, slot3_memory, 2),
-
     "slot1_add_1",
     Inst.LBEQ(slot1_counter, counter_max, "slot1_reset"),
-    Inst.ADDI(slot1_counter, slot1_counter, 1),  
+    Inst.ADDI(slot1_counter, slot1_counter, 1),
     Inst.LJAL(0, "slot2_add_1"),  # goto slot2_add_1
     "slot1_reset",
     Inst.ADDI(slot1_counter, 0, 0),
     Inst.LJAL(0, "slot2_add_1"),  # goto slot2_add_1
-
     "slot2_add_1",
     Inst.LBEQ(slot2_counter, counter_max, "slot2_reset"),
-    Inst.ADDI(slot2_counter, slot2_counter, 1),  
+    Inst.ADDI(slot2_counter, slot2_counter, 1),
     Inst.LJAL(0, "slot3_add_1"),  # goto slot3_add_1
     "slot2_reset",
     Inst.ADDI(slot2_counter, 0, 0),
     Inst.LJAL(0, "slot3_add_1"),  # goto slot3_add_1
-
     "slot3_add_1",
     Inst.LBEQ(slot3_counter, counter_max, "slot3_reset"),
-    Inst.ADDI(slot3_counter, slot3_counter, 1),  
+    Inst.ADDI(slot3_counter, slot3_counter, 1),
     Inst.LJAL(0, "loop"),  # goto loop
-    
     "slot3_reset",
     Inst.ADDI(slot3_counter, 0, 0),
-    Inst.LJAL(0, "slot3_add_1") # goto loop
+    Inst.LJAL(0, "slot3_add_1"),  # goto loop
 ]
 
 program.extend(program_main)
